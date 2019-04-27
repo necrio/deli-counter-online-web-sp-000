b966f1f906d3dfd5eff059896573d55010927bf0
Keith Isham <kisham8899@gmail.com>
2019-04-27 14:15:47 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line(katz_deli)
 
   when 0
     puts "The line is currently empty."
-  else 
+  else
     puts "The line is currently: 1. #{katz_deli[0]} 2. #{katz_deli[1]} 3. #{katz_deli[2]}"
   end
 
