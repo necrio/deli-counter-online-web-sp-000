7428a0505052fb988f8097785561f9910115f26a
Keith Isham <kisham8899@gmail.com>
2019-04-27 14:49:37 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -25,7 +25,7 @@ def now_serving(line)
     if line.length == 0
       puts "There is nobody waiting to be served!"
     else
-      puts "Currently serving #{line.first}."
+      puts "Currently serving #{line[0]}."
     end
 
 end
